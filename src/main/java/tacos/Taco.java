a0e6e10f581bdef5299887dd44933faefcf2902c
BogdanMykhalchhuk <bogden1979@yahoo.com>
2018-03-19 08:47:14 +0200

    'master'

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -2,7 +2,10 @@ package tacos;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Taco {
     private String name;
+    private List<Ingredient> ingredients;
 }
--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tacos.Design;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
 import tacos.Taco;
@@ -44,4 +46,12 @@ public class DesignTacoController {
         return "design";
     }
 
+    @PostMapping
+    public String processDesign(Design design) {
+        // Save the taco design...
+        // We'll do this in chapter 3
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
+    }
 }
